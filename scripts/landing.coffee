f5729813b571670332cb5b5f095130706dfe2f14
Matthew Harris <mharr171.z@gmail.com>
2017-07-31 22:34:08 -0700

    assignment-8: completed

--- scripts/landing.coffee
+++ scripts/landing.coffee
@@ -1,25 +1,56 @@
-animatePoints = () ->
+animatePoints = (choice = 1) ->
   points = document.getElementsByClassName('point');
 
-  revealFirstPoint = () ->
-    points[0].style.opacity = 1;
-    points[0].style.transform = "scaleX(1) translateY(0)"
-    points[0].style.msTransform = "scaleX(1) translateY(0)"
-    points[0].style.WebkitTransform = "scaleX(1) translateY(0)"
-
-  revealSecondPoint = () ->
-    points[1].style.opacity = 1;
-    points[1].style.transform = "scaleX(1) translateY(0)"
-    points[1].style.msTransform = "scaleX(1) translateY(0)"
-    points[1].style.WebkitTransform = "scaleX(1) translateY(0)"
-
-  revealThirdPoint = () ->
-    points[2].style.opacity = 1;
-    points[2].style.transform = "scaleX(1) translateY(0)"
-    points[2].style.msTransform = "scaleX(1) translateY(0)"
-    points[2].style.WebkitTransform = "scaleX(1) translateY(0)"
-
-  revealFirstPoint()
-  revealSecondPoint()
-  revealThirdPoint()
+  if choice == 1
+    revealPoint = (points) ->
+      i = 0
+      while i < points.length
+        points[i].style.opacity = 1
+        points[i].style.transform = "scaleX(1) translateY(0)"
+        points[i].style.msTransform = "scaleX(1) translateY(0)"
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)"
+        if i == 1
+          points[i].style.opacity = 1
+          points[i].style.transform = "scaleX(1) translateY(0)"
+          points[i].style.msTransform = "scaleX(1) translateY(0)"
+          points[i].style.WebkitTransform = "scaleX(1) translateY(0)"
+          points[i].style.transition =  "all 0.3s ease-in-out"
+          points[i].style.msTransition = "all 0.3s ease-in-out"
+          points[i].style.WebkitTransition = "all 0.3s ease-in-out"
+          points[i].style.transitionDelay = "0.15s"
+          points[i].style.msTransitionDelay = "0.15s"
+          points[i].style.WebkitTransitionDelay = "0.15s"
+        i++
+      return
+    revealPoint(points)
+  else if choice == 2
+    hidePoint = (points) ->
+      i = 0
+      while i < points.length
+        points[i].style.transition =  "all 0.15s ease-in-out"
+        points[i].style.msTransition = "all 0.15s ease-in-out"
+        points[i].style.WebkitTransition = "all 0.15s ease-in-out"
+        points[i].style.opacity = 0
+        points[i].style.transform = "scaleX(1.5) scaleY(.01) translateY(3rem)"
+        points[i].style.msTransform = "scaleX(1.5) scaleY(.01) translateY(3rem)"
+        points[i].style.WebkitTransform = "scaleX(1.5) scaleY(.01) translateY(3rem)"
+        i++
+      return
+    resetPoint = (points) ->
+      i = 0
+      while i < points.length
+        points[i].style.transition =  "all 0.25s ease-in-out"
+        points[i].style.msTransition = "all 0.25s ease-in-out"
+        points[i].style.WebkitTransition = "all 0.25s ease-in-out"
+        points[i].style.transform = "scaleX(.9) scaleY(1) translateY(3rem)"
+        points[i].style.msTransform = "scaleX(.9) scaleY(1) translateY(3rem)"
+        points[i].style.WebkitTransform = "scaleX(.9) scaleY(1) translateY(3rem)"
+        i++
+      return
+    hidePoint(points)
+    #resetPoint(points)
+    setTimeout(()->
+      resetPoint(points)
+      return
+    ,1000)
   return
--- scripts/landing.js
+++ scripts/landing.js
@@ -1,27 +1,67 @@
 var animatePoints;
 
-animatePoints = function() {
-  var points, revealFirstPoint, revealSecondPoint, revealThirdPoint;
+animatePoints = function(choice) {
+  var hidePoint, points, resetPoint, revealPoint;
+  if (choice == null) {
+    choice = 1;
+  }
   points = document.getElementsByClassName('point');
-  revealFirstPoint = function() {
-    points[0].style.opacity = 1;
-    points[0].style.transform = "scaleX(1) translateY(0)";
-    points[0].style.msTransform = "scaleX(1) translateY(0)";
-    return points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-  revealSecondPoint = function() {
-    points[1].style.opacity = 1;
-    points[1].style.transform = "scaleX(1) translateY(0)";
-    points[1].style.msTransform = "scaleX(1) translateY(0)";
-    return points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-  revealThirdPoint = function() {
-    points[2].style.opacity = 1;
-    points[2].style.transform = "scaleX(1) translateY(0)";
-    points[2].style.msTransform = "scaleX(1) translateY(0)";
-    return points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-  revealFirstPoint();
-  revealSecondPoint();
-  revealThirdPoint();
+  if (choice === 1) {
+    revealPoint = function(points) {
+      var i;
+      i = 0;
+      while (i < points.length) {
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.msTransform = "scaleX(1) translateY(0)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+        if (i === 1) {
+          points[i].style.opacity = 1;
+          points[i].style.transform = "scaleX(1) translateY(0)";
+          points[i].style.msTransform = "scaleX(1) translateY(0)";
+          points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+          points[i].style.transition = "all 0.3s ease-in-out";
+          points[i].style.msTransition = "all 0.3s ease-in-out";
+          points[i].style.WebkitTransition = "all 0.3s ease-in-out";
+          points[i].style.transitionDelay = "0.15s";
+          points[i].style.msTransitionDelay = "0.15s";
+          points[i].style.WebkitTransitionDelay = "0.15s";
+        }
+        i++;
+      }
+    };
+    revealPoint(points);
+  } else if (choice === 2) {
+    hidePoint = function(points) {
+      var i;
+      i = 0;
+      while (i < points.length) {
+        points[i].style.transition = "all 0.15s ease-in-out";
+        points[i].style.msTransition = "all 0.15s ease-in-out";
+        points[i].style.WebkitTransition = "all 0.15s ease-in-out";
+        points[i].style.opacity = 0;
+        points[i].style.transform = "scaleX(1.5) scaleY(.01) translateY(3rem)";
+        points[i].style.msTransform = "scaleX(1.5) scaleY(.01) translateY(3rem)";
+        points[i].style.WebkitTransform = "scaleX(1.5) scaleY(.01) translateY(3rem)";
+        i++;
+      }
+    };
+    resetPoint = function(points) {
+      var i;
+      i = 0;
+      while (i < points.length) {
+        points[i].style.transition = "all 0.25s ease-in-out";
+        points[i].style.msTransition = "all 0.25s ease-in-out";
+        points[i].style.WebkitTransition = "all 0.25s ease-in-out";
+        points[i].style.transform = "scaleX(.9) scaleY(1) translateY(3rem)";
+        points[i].style.msTransform = "scaleX(.9) scaleY(1) translateY(3rem)";
+        points[i].style.WebkitTransform = "scaleX(.9) scaleY(1) translateY(3rem)";
+        i++;
+      }
+    };
+    hidePoint(points);
+    setTimeout(function() {
+      resetPoint(points);
+    }, 1000);
+  }
 };
