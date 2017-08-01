ba78aeed481d98d4e2292ec6b09ce8d846695116
Matthew Harris <mharr171.z@gmail.com>
2017-07-31 19:56:40 -0700

    checkpoint-8: completed

---
+++ scripts/landing.js
@@ -0,0 +1,27 @@
+var animatePoints;
+
+animatePoints = function() {
+  var points, revealFirstPoint, revealSecondPoint, revealThirdPoint;
+  points = document.getElementsByClassName('point');
+  revealFirstPoint = function() {
+    points[0].style.opacity = 1;
+    points[0].style.transform = "scaleX(1) translateY(0)";
+    points[0].style.msTransform = "scaleX(1) translateY(0)";
+    return points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+  };
+  revealSecondPoint = function() {
+    points[1].style.opacity = 1;
+    points[1].style.transform = "scaleX(1) translateY(0)";
+    points[1].style.msTransform = "scaleX(1) translateY(0)";
+    return points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
+  };
+  revealThirdPoint = function() {
+    points[2].style.opacity = 1;
+    points[2].style.transform = "scaleX(1) translateY(0)";
+    points[2].style.msTransform = "scaleX(1) translateY(0)";
+    return points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
+  };
+  revealFirstPoint();
+  revealSecondPoint();
+  revealThirdPoint();
+};
