e7a33bc4711c5e48f004028ebd1956e29d3d6a10
Matthew Harris <mharr171.z@gmail.com>
2017-08-08 03:45:23 -0700

    fix animations

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,64 +1,74 @@
-var animatePoints, hidePoint, pointsArray, resetPoint, revealPoint;
-
 pointsArray = document.getElementsByClassName('point');
 
-revealPoint = function(element) {
-  element.style.opacity = 1;
-  element.style.transform = 'scaleX(1) translateY(0)';
-  element.style.msTransform = 'scaleX(1) translateY(0)';
-  element.style.WebkitTransform = 'scaleX(1) translateY(0)';
-  element.style.transition = 'all 0.3s ease-in-out';
-  element.style.msTransition = 'all 0.3s ease-in-out';
-  element.style.WebkitTransition = 'all 0.3s ease-in-out';
-  element.style.transitionDelay = '0.1s';
-  element.style.msTransitionDelay = '0.1s';
-  element.style.WebkitTransitionDelay = '0.1s';
+
+var revealPoint = function(point) {
+  for(var i = 0; i < point.length; i++){
+    point[i].style.opacity = 1;
+
+    point[i].style.transition = 'all 0.3s ease-in-out';
+    point[i].style.msTransition = 'all 0.3s ease-in-out';
+    point[i].style.WebkitTransition = 'all 0.3s ease-in-out';
+
+    point[i].style.transform = 'scaleX(1) translateY(0)';
+    point[i].style.msTransform = 'scaleX(1) translateY(0)';
+    point[i].style.WebkitTransform = 'scaleX(1) translateY(0)';
+
+    point[i].style.transitionDelay = '0.15s';
+    point[i].style.msTransitionDelay = '0.15s';
+    point[i].style.WebkitTransitionDelay = '0.15s';
+  }
 };
 
-hidePoint = function(element) {
-  element.style.opacity = 0;
-  element.style.transform = 'scaleX(1.5) scaleY(.01) translateY(8rem)';
-  element.style.msTransform = 'scaleX(1.5) scaleY(.01) translateY(8rem)';
-  element.style.WebkitTransform = 'scaleX(1.5) scaleY(.01) translateY(8rem)';
-  element.style.transition = 'all .2s ease-in-out';
-  element.style.msTransition = 'all .2s ease-in-out';
-  element.style.WebkitTransition = 'all .2s ease-in-out';
+var hidePoint = function(point) {
+  for(var i = 0; i < point.length; i++){
+    point[i].style.opacity = 0;
+
+    point[i].style.transition = 'all 0.15s ease-in-out';
+    point[i].style.msTransition = 'all 0.15s ease-in-out';
+    point[i].style.WebkitTransition = 'all 0.15s ease-in-out';
+
+    point[i].style.transform = 'scaleX(1.5) scaleY(.01) translateY(3rem)';
+    point[i].style.msTransform = 'scaleX(1.5) scaleY(.01) translateY(3rem)';
+    point[i].style.WebkitTransform = 'scaleX(1.5) scaleY(.01) translateY(3rem)';
+
+    point[i].style.transitionDelay = '0s';
+    point[i].style.msTransitionDelay = '0s';
+    point[i].style.WebkitTransitionDelay = '0s';
+  }
 };
 
-resetPoint = function(element) {
-  element.style.transitionDelay = '0s';
-  element.style.msTransitionDelay = '0';
-  element.style.WebkitTransitionDelay = '0';
-  element.style.transform = 'scaleX(.1) scaleY(1) translateY(3rem)';
-  element.style.msTransform = 'scaleX(.1) scaleY(1) translateY(3rem)';
-  element.style.WebkitTransform = 'scaleX(.9) scaleY(1) translateY(3rem)';
-  element.style.transition = 'all 0s ease-in-out';
-  element.style.msTransition = 'all 0s ease-in-out';
-  element.style.WebkitTransition = 'all 0s ease-in-out';
+var resetPoint = function(point) {
+  for(var i = 0; i < point.length; i++){
+    point[i].style.transition = 'all 0s ease-in-out';
+    point[i].style.msTransition = 'all 0s ease-in-out';
+    point[i].style.WebkitTransition = 'all 0s ease-in-out';
+
+    point[i].style.transform = 'scaleX(.9) scaleY(1) translateY(3rem)';
+    point[i].style.msTransform = 'scaleX(.9) scaleY(1) translateY(3rem)';
+    point[i].style.WebkitTransform = 'scaleX(.9) scaleY(1) translateY(3rem)';
+  }
 };
 
-animatePoints = function(pointsArray, choice) {
+var animatePoints = function(pointsArray, choice) {
   if (choice == null) {
     choice = 1;
   }
   if (choice === 1) {
-    forEach(pointsArray, revealPoint);
+    revealPoint(pointsArray);
   } else if (choice === 2) {
-    forEach(pointsArray, hidePoint);
-    setTimeout(function() {
-      return forEach(pointsArray, resetPoint);
-    }, 1000);
+    hidePoint(pointsArray);
+
+    setTimeout(function() {resetPoint(pointsArray);}, 1000);
   }
 };
 
 window.onload = function() {
-  var scrollDistance, sellingPoints;
   if (window.innerHeight > 950) {
     animatePoints(pointsArray);
   }
-  sellingPoints = document.getElementsByClassName('selling-points')[0];
-  scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-  return window.addEventListener('scroll', function(e) {
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  return window.addEventListener('scroll', function(event) {
     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
       animatePoints(pointsArray);
     }
